61080f5dafc5992281b80c316a95e4c0df69aac4
myh <curryhuang3627@gmail.com>
2023-02-10 15:50:50 +0800

    Initial Commit

---
+++ ToDDoCore/ToDDoCore.h
@@ -0,0 +1,18 @@
+//
+//  ToDDoCore.h
+//  ToDDoCore
+//
+//  Created by Min-Yang Huang on 2023/2/10.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ToDDoCore.
+FOUNDATION_EXPORT double ToDDoCoreVersionNumber;
+
+//! Project version string for ToDDoCore.
+FOUNDATION_EXPORT const unsigned char ToDDoCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ToDDoCore/PublicHeader.h>
+
+
