7447dfa92542ad32328c94fbcaba598bb4fa759c
myh <curryhuang3627@gmail.com>
2023-02-10 17:24:40 +0800

    Load command request store retrieval

--- ToDDoCoreTests/FeedLoaderUseCaseTests.swift
+++ ToDDoCoreTests/FeedLoaderUseCaseTests.swift
@@ -8,8 +8,20 @@
 import XCTest
 import ToDDoCore
 
+protocol FeedStore {
+    func retrieve()
+}
+
 class FeedLoader {
+    let store: FeedStore
     
+    init(store: FeedStore) {
+        self.store = store
+    }
+    
+    func load() {
+        store.retrieve()
+    }
 }
 
 class FeedLoaderUserCaseTests: XCTestCase {
@@ -20,18 +32,32 @@ class FeedLoaderUserCaseTests: XCTestCase {
         XCTAssertEqual(store.receivedMessage, [])
     }
     
+    func test_load_requestsStoreRetrieval() {
+        let (sut, store) = makeSUT()
+        
+        sut.load()
+        
+        XCTAssertEqual(store.receivedMessage, [.retrieve])
+    }
+    
     //MARK: - Helpers
     
     private func makeSUT() -> (sut: FeedLoader, store: FeedStoreSpy) {
-        let sut = FeedLoader()
         let store = FeedStoreSpy()
+        let sut = FeedLoader(store: store)
         return (sut, store)
     }
     
-    private class FeedStoreSpy {
-        let receivedMessage = [ReceivedMessage]()
+    private class FeedStoreSpy: FeedStore {
+        var receivedMessage = [ReceivedMessage]()
         
-        enum ReceivedMessage: Equatable {}
+        enum ReceivedMessage: Equatable {
+            case retrieve
+        }
+        
+        func retrieve() {
+            receivedMessage.append(.retrieve)
+        }
     }
     
 }
