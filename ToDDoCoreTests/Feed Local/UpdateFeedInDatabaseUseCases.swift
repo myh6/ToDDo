43b6d774db8ebbceea1aba13a801f1b032c8b8a4
myh6 <curryhuang3627@gmail.com>
2023-02-16 16:47:32 +0800

    LocalFeedLoader does not message store upon creation (before updating feed)

---
+++ ToDDoCoreTests/Feed Local/UpdateFeedInDatabaseUseCases.swift
@@ -0,0 +1,27 @@
+//
+//  UpdateFeedInDatabaseUseCases.swift
+//  ToDDoCoreTests
+//
+//  Created by Min-Yang Huang on 2023/2/16.
+//
+
+import XCTest
+import ToDDoCore
+
+class UpdateFeedInDatabaseUseCases: XCTestCase {
+    
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSUT()
+        
+        XCTAssertTrue(store.receivedMessage.isEmpty)
+    }
+    
+    //MARK: - Helpers
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
+        let store = FeedStoreSpy()
+        let sut = LocalFeedLoader(store: store)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
+}
