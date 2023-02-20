38fc4c898e7486195b38a270934ef648e95ce405
myh6 <curryhuang3627@gmail.com>
2023-02-20 15:49:24 +0800

    Load persistent container stores upon CoreDataFeedStore initialization

--- ToDDoCore/Feed Local/Infrastructure/CoreDataFeedStore.swift
+++ ToDDoCore/Feed Local/Infrastructure/CoreDataFeedStore.swift
@@ -5,10 +5,15 @@
 //  Created by Min-Yang Huang on 2023/2/20.
 //
 
-import Foundation
+import CoreData
 
 public class CoreDataFeedStore: FeedStore {
-    public init() {}
+    
+    private let container: NSPersistentContainer
+    
+    public init(bundle: Bundle = .main) throws {
+        container = try NSPersistentContainer.load(modelName: "ToDDoStore", in: bundle)
+    }
     
     public func retrieve(completion: @escaping RetrievalCompletion) {
         completion(.success(.none))
@@ -31,3 +36,32 @@ public class CoreDataFeedStore: FeedStore {
     }
     
 }
+
+private extension NSPersistentContainer {
+    enum LoadingError: Swift.Error {
+        case modelNotFound
+        case failedToLoadPersistentStores(Swift.Error)
+    }
+    
+    static func load(modelName name: String, in bundle: Bundle) throws -> NSPersistentContainer {
+        guard let model = NSManagedObjectModel.with(name: name, in: bundle) else {
+            throw LoadingError.modelNotFound
+        }
+        
+        let container = NSPersistentContainer(name: name, managedObjectModel: model)
+        var loadError: Swift.Error?
+        container.loadPersistentStores { loadError = $1 }
+        try loadError.map { throw LoadingError.failedToLoadPersistentStores($0) }
+        
+        return container
+    }
+}
+
+private extension NSManagedObjectModel {
+    static func with(name: String, in bundle: Bundle) -> NSManagedObjectModel? {
+        return bundle
+            .url(forResource: name, withExtension: "momd")
+            .flatMap { NSManagedObjectModel(contentsOf: $0) }
+    }
+}
+
--- ToDDoCoreTests/Feed Local/CoreDataFeedStoreTests.swift
+++ ToDDoCoreTests/Feed Local/CoreDataFeedStoreTests.swift
@@ -41,9 +41,14 @@ class CoreDataFeedStoreTests: XCTestCase {
         }
     }
     
+    func test_retrieve_deliversFoundValueOnNonEmptyDatabase() {
+        
+    }
+    
     //MARK: - Helpers
     private func makeSUT() -> FeedStore {
-        let sut = CoreDataFeedStore()
+        let storeBundle = Bundle(for: CoreDataFeedStore.self)
+        let sut = try! CoreDataFeedStore(bundle: storeBundle)
         trackForMemoryLeaks(sut)
         return sut
     }
