52c582f815682aa3b7a79b7bf1d7851e3a2e3c4d
myh6 <curryhuang3627@gmail.com>
2023-02-20 14:46:17 +0800

    CoreDataFeedStore delivers empty on empty database

---
+++ ToDDoCoreTests/Feed Local/CoreDataFeedStoreTests.swift
@@ -0,0 +1,51 @@
+//
+//  CoreDataFeedStoreTests.swift
+//  ToDDoCoreTests
+//
+//  Created by Min-Yang Huang on 2023/2/20.
+//
+
+import XCTest
+import ToDDoCore
+
+class CoreDataFeedStore: FeedStore {
+    func retrieve(completion: @escaping RetrievalCompletion) {
+        completion(.success(.none))
+    }
+    
+    func insert(_ feed: ToDDoCore.LocalFeedListGroup, completion: @escaping InsertionCompletion) {
+        
+    }
+    
+    func remove(_ feed: ToDDoCore.LocalFeedListGroup, completion: @escaping RemovalCompletion) {
+        
+    }
+    
+    func update(_ feed: ToDDoCore.LocalFeedListGroup, completion: @escaping UpdateCompletion) {
+        
+    }
+    
+    func hasItem(withID: UUID) -> Bool {
+        return false
+    }
+    
+}
+
+class CoreDataFeedStoreTests: XCTestCase {
+    
+    func test_retrieve_deliversEmptyOnEmptyDatabase() {
+        let sut = CoreDataFeedStore()
+        
+        sut.retrieve { result in
+            if case let .success(item) = result {
+                XCTAssertNil(item)
+            } else {
+                XCTFail("Expected no item delivered from empty database")
+            }
+        }
+    }
+    
+    
+    
+    
+}
