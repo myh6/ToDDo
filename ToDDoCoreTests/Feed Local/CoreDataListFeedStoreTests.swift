386e04e3a767b210b778c31611c3e7c56d9b0bab
myh6 <curryhuang3627@gmail.com>
2023-03-13 11:07:16 +0800

    (filePath: ) API is only available to macOS 13.0. Use fileURLWithPath instead

--- ToDDoCoreTests/Feed Local/CoreDataListFeedStoreTests.swift
+++ ToDDoCoreTests/Feed Local/CoreDataListFeedStoreTests.swift
@@ -278,7 +278,7 @@ class CoreDataListFeedStoreTests: XCTestCase {
     //MARK: - Helpers
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> FeedStore {
         let storeBundle = Bundle(for: CoreDataFeedStore.self)
-        let storeURL = URL(filePath: "/dev/null")
+        let storeURL = URL(fileURLWithPath: "/dev/null")
         let sut = try! CoreDataFeedStore(storeURL: storeURL, bundle: storeBundle)
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
