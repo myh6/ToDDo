f5833f347e6a2d2f2fbae21449a0f095c74700e4
myh6 <curryhuang3627@gmail.com>
2023-03-23 15:07:40 +0800

    Load feed in viewDidLoad

--- ToDDoiOSPresentationTests/ToDDoiOSPresentationTests.swift
+++ ToDDoiOSPresentationTests/ToDDoiOSPresentationTests.swift
@@ -7,11 +7,20 @@
 
 import XCTest
 import ToDDoiOSPresentation
+import UIKit
 
-final class FeedViewController {
+final class FeedViewController: UIViewController {
     
-    init(loader: ToDDoiOSPresentationTests.LoaderSpy) {
-        
+    private var loader: ToDDoiOSPresentationTests.LoaderSpy?
+    
+    convenience init(loader: ToDDoiOSPresentationTests.LoaderSpy) {
+        self.init()
+        self.loader = loader
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loader?.load()
     }
 }
 
@@ -26,8 +35,21 @@ final class ToDDoiOSPresentationTests: XCTestCase {
         }
     }
     
+    func test_viewDidLoad_loadsFeed() {
+        let loader = LoaderSpy()
+        let sut = FeedViewController(loader: loader)
+        
+        sut.loadViewIfNeeded()
+        
+        XCTAssertEqual(loader.loadCallCount, 1)
+    }
+    
     class LoaderSpy {
         private(set) var loadCallCount: Int = 0
+        
+        func load() {
+            loadCallCount += 1
+        }
     }
 
 }
