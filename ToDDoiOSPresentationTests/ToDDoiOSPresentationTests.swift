1c7c7b648234684d645b6c394299b112fa80755a
myh6 <curryhuang3627@gmail.com>
2023-03-23 15:11:27 +0800

    Extract system under test creation to a factory method

--- ToDDoiOSPresentationTests/ToDDoiOSPresentationTests.swift
+++ ToDDoiOSPresentationTests/ToDDoiOSPresentationTests.swift
@@ -28,21 +28,29 @@ final class ToDDoiOSPresentationTests: XCTestCase {
 
     func test_init_doesNotLoadFeed() {
         func test_init_doesNotLoadFeed() {
-            let loader = LoaderSpy()
-            _ = FeedViewController(loader: loader)
+            let (_ ,loader) = makeSUT()
             
             XCTAssertEqual(loader.loadCallCount, 0)
         }
     }
     
     func test_viewDidLoad_loadsFeed() {
-        let loader = LoaderSpy()
-        let sut = FeedViewController(loader: loader)
+        let (sut, loader) = makeSUT()
         
         sut.loadViewIfNeeded()
         
         XCTAssertEqual(loader.loadCallCount, 1)
     }
+    
+    //MARK: - Helpers
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedViewController, loader: LoaderSpy) {
+        let loader = LoaderSpy()
+        let sut = FeedViewController(loader: loader)
+        trackForMemoryLeaks(loader, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, loader)
+    }
+    
     
     class LoaderSpy {
         private(set) var loadCallCount: Int = 0
