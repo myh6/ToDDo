744e283a84b6a3f474222565d96dadec8ed6ed25
myh6 <curryhuang3627@gmail.com>
2023-03-23 14:04:31 +0800

    Add 'ToDDoiOSPresentation' framework target for iOS-platform specific components

---
+++ ToDDoiOSPresentation/ToDDoiOSPresentation.h
@@ -0,0 +1,18 @@
+//
+//  ToDDoiOSPresentation.h
+//  ToDDoiOSPresentation
+//
+//  Created by Min-Yang Huang on 2023/3/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ToDDoiOSPresentation.
+FOUNDATION_EXPORT double ToDDoiOSPresentationVersionNumber;
+
+//! Project version string for ToDDoiOSPresentation.
+FOUNDATION_EXPORT const unsigned char ToDDoiOSPresentationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ToDDoiOSPresentation/PublicHeader.h>
+
+
---
+++ ToDDoiOSPresentationTests/ToDDoiOSPresentationTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDDoiOSPresentationTests.swift
+//  ToDDoiOSPresentationTests
+//
+//  Created by Min-Yang Huang on 2023/3/23.
+//
+
+import XCTest
+import ToDDoiOSPresentation
+
+final class ToDDoiOSPresentationTests: XCTestCase {
+
+    func test_init() {
+        
+    }
+
+}
