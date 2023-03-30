083f802af4ee12ea6e6bf03756bd55a6819bc504
myh6 <curryhuang3627@gmail.com>
2023-03-30 14:19:35 +0800

    Create a custom ErrorView with retry button

---
+++ ToDDoiOSPresentation/SwiftUI/Helper/ErrorView.swift
@@ -0,0 +1,32 @@
+//
+//  ErrorView.swift
+//  ToDDoiOSPresentation
+//
+//  Created by Min-Yang Huang on 2023/3/30.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    let message: String
+    let retry: () -> Void
+    var body: some View {
+        Button {
+            retry()
+        } label: {
+            ZStack {
+                Rectangle()
+                    .foregroundColor(Color(hue: 1.0, saturation: 0.402, brightness: 0.702))
+                Text(message)
+                    .foregroundColor(Color.white)
+            }
+        }
+    }
+}
+
+struct ErrorView_Previews: PreviewProvider {
+    static var previews: some View {
+        ErrorView(message: "Error! Please refresh", retry: {})
+            .previewLayout(.sizeThatFits)
+    }
+}
