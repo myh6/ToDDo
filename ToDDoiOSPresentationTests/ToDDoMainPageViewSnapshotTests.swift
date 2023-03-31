5adbcbfc23612d9acb7e6cfa93bb6d6e9f589718
myh6 <curryhuang3627@gmail.com>
2023-03-31 11:37:44 +0800

    Add ErrorView snapshot

--- ToDDoiOSPresentationTests/ToDDoMainPageViewSnapshotTests.swift
+++ ToDDoiOSPresentationTests/ToDDoMainPageViewSnapshotTests.swift
@@ -13,23 +13,37 @@ import SwiftUI
 class ToDDoMainPageViewSnapshotTests: XCTestCase {
     
     func test_emptyView() {
-        let sut = makeSUT()
+        let sut = makeSUT(loader: LoaderFakeWithEmptyData())
         
         assert(snapshot: sut.snapshot(for: .iPhone13(style: .light)), named: "EMPTY_VIEW_light")
         assert(snapshot: sut.snapshot(for: .iPhone13(style: .dark)), named: "EMPTY_VIEW_dark")
     }
     
+    func test_errorView() {
+        let sut = makeSUT(loader: LoaderFakeWithError())
+        
+        assert(snapshot: sut.snapshot(for: .iPhone13(style: .light)), named: "ERROR_VIEW_light")
+        assert(snapshot: sut.snapshot(for: .iPhone13(style: .dark)), named: "ERROR_VIEW_dark")
+    }
+    
     //MARK: - Helpers
-    private func makeSUT() -> UIHostingController<ToDDoMainPageView> {
+    private func makeSUT(loader: FeedLoader, file: StaticString = #file, line: UInt = #line) -> UIHostingController<ToDDoMainPageView> {
         let store = SelectableMenuStore(options: ["Recent", "Pending", "Finished"], didSelect: { _ in})
-        let viewModel = ToDDoMainViewModel(date: renderExactDate(), loader: LoaderFake(), timezone: TimeZone(identifier: "UTC")!, locale: Locale(identifier: "en_US_POSIX"))
+        let viewModel = ToDDoMainViewModel(date: renderExactDate(), loader: loader, timezone: TimeZone(identifier: "UTC")!, locale: Locale(identifier: "en_US_POSIX"))
         let sut = UIHostingController(rootView: ToDDoMainPageView(viewModel: viewModel, store: store))
         return sut
     }
     
-    private class LoaderFake: FeedLoader {
+    private class LoaderFakeWithEmptyData: FeedLoader {
         func load(completion: @escaping (FeedLoader.Result) -> Void) {
             completion(.success(nil))
+        }
+    }
+    
+    private class LoaderFakeWithError: FeedLoader {
+        func load(completion: @escaping (FeedLoader.Result) -> Void) {
+            let error = NSError(domain: "An Error", code: 0)
+            completion(.failure(error))
         }
     }
 }
